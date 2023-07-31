f31a282ff13ae034b1073a17137b6886a7af2a55
dkpapp <118980727+dkpapp@users.noreply.github.com>
2023-07-31 19:51:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6019972009:AAEAfErAbjFpJnwFfII5K1oPikw0kSR
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/f9ff43ac9a72c4dfd75c1.jpg https://telegra.ph/file/f6da7741e57daac351a8b.jpg https://telegra.ph/file/c51248450e539480953ce.jpg https://telegra.ph/file/98842f9fe8978a8688b9c.jpg https://telegra.ph/file/139e874011a7f6175c3ef.jpg https://telegra.ph/file/9f4137c00d4daac7bb5fd.jpg https://telegra.ph/file/5915c9a73683189e8aafb.jpg https://telegra.ph/file/643ff05841fae4e1519a7.jpg https://telegra.ph/file/31fa390308ef95aa6f198.jpg https://telegra.ph/file/643ff05841fae4e1519a7.jpg https://telegra.ph/file/98842f9fe8978a8688b9c.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/b806ad314d0c415571bde.jpg'))
+#PIC = (environ.get('PICS' ,'https://telegra.ph/file/f9ff43ac9a72c4dfd75c1.jpg https://telegra.ph/file/f6da7741e57daac351a8b.jpg https://telegra.ph/file/c51248450e539480953ce.jpg https://telegra.ph/file/98842f9fe8978a8688b9c.jpg https://telegra.ph/file/139e874011a7f6175c3ef.jpg https://telegra.ph/file/9f4137c00d4daac7bb5fd.jpg https://telegra.ph/file/5915c9a73683189e8aafb.jpg https://telegra.ph/file/643ff05841fae4e1519a7.jpg https://telegra.ph/file/31fa390308ef95aa6f198.jpg https://telegra.ph/file/643ff05841fae4e1519a7.jpg https://telegra.ph/file/98842f9fe8978a8688b9c.jpg')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
