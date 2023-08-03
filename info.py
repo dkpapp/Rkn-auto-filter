d3d1f97af07c59ab5c8b0cd1a24f94df3f77b5d5
dkpapp <118980727+dkpapp@users.noreply.github.com>
2023-08-03 07:48:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,9 +37,9 @@ PICS = (environ.get('PICS' ,'https://telegra.ph/file/b806ad314d0c415571bde.jpg h
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2067727305', '2103555754').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2067727305 2103555754').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '2067727305').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '2067727305 2103555754').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '-1001957984770')
 auth_grp = environ.get('AUTH_GROUP', '-1001517645234')
